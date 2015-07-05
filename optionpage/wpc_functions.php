45deb159fdf449fd302f396a215f7e930159c4b9
Davinder Singh <honeydavinder@gmail.com>
2015-07-05 15:41:19 +0530

    Remove unwanted files

--- optionpage/wpc_functions.php
+++
@@ -1,9 +0,0 @@
-<?php 
-global $wpdb;
-
-function getFields($sectionID){
-	global $wpdb;
-	$GetfieldsArray = $wpdb->get_results( "SELECT * FROM wp_optionspage_fields where sectionID = $sectionID" , OBJECT );
-	return $GetfieldsArray;
-}
-?>
